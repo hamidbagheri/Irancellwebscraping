876595207b1d4d9f625a5739296c3d48d63a42dd
Hamid <h.bagheri@live.com>
2021-11-13 00:56:59 +0330

    aftet add list for figing song name

--- sql.py
+++ sql.py
@@ -1,17 +1,22 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    # host="localhost",
+    host="localhost",
     user="root",
     password="",
     database='webscraping'
 )
 mycursor = mydb.cursor()
+# mycursor.execute(
+#     "CREATE TABLE rbt (ID INT(10),Singer_Name VARCHAR(255)"
+#     ", Song_name VARCHAR(255),Song_Price VARCHAR(255),"
+#     "Song_Code VARCHAR(255),Song_Period VARCHAR(255))"
+#     )
 
-sql = "INSERT INTO Persons (FirstName, LastName) VALUES (%s, %s)"
-val = ("John", "Highway")
-mycursor.execute(sql, val)
+# sql = "INSERT INTO Persons (Singer_Name, Song_name,Song_Price,Song_Code,Song_Period) VALUES (%s, %s,%s,%s,%s)"
+# val = ("John", "Highway")
+# mycursor.execute(sql, val)
 
 mydb.commit()
 
-print(mycursor.rowcount, "record inserted.")
+# print(mycursor.rowcount, "record inserted.")
--- web-dict.py
+++ web-dict.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database='webscraping'
+)
 
 RBT_list = []
 urlLists = []
@@ -13,34 +21,37 @@ for url in urlLists:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     parent = soup.find('ul', class_="songList songlist-table songlist-content rtl")
-    song_name = parent.li.div.find('div', class_='t-info').a.contents[0]
     for x in parent.find_all('li'):
         for y in x.find_all('div', class_=['t-info']):
             Singer_Name = y.find('a', class_='singer-txt ellipsis').contents[0]
-
         for y in x.find_all('div', class_=['t-info']):
-            Song_name = y.find('a', class_='song-txt ellipsis').contents[0]
-
+            Song_Name = y.find('a', class_='song-txt ellipsis').contents[0]
         for y in x.find_all('div', class_=['t-price']):
             Song_Price = y.p.contents[0]
-
         for y in x.find_all('div', class_=['t-code']):
             Song_Code = y.p.contents[0]
         for y in x.find_all('div', class_=['t-period']):
             Song_Period = y.p.contents[0]
 
         RBT_Dict = {'Singer_Name': Singer_Name,
-                    'Song_name': Song_name,
+                    'Song_Name': Song_Name,
                     'Song_Price': Song_Price,
-                    'Song_Code': Song_Code,
+                    'Song_Code': int(Song_Code),
                     'Song_Period': Song_Period,
                     }
-        # print(RBT_Dict)
         RBT_list.append(RBT_Dict)
-print(RBT_list)
-for x in RBT_list:
-    print(x.pop('Singer_Name'))
-    print(x.pop('Song_name'))
-    print(int(x.pop('Song_Code')))
-    print('*******')
 
+        sql = "insert into rbt(Singer_Name, Song_Name,Song_Price,Song_Code,Song_Period) ' \
+                'values ('{0}','{1}','{2}','{3}','{4}');".format(Singer_Name, Song_Name, Song_Price, Song_Code,
+                                                                 Song_Period)
+
+for item in range(len(RBT_list)):
+    sql = "insert into rbt(Singer_Name, Song_Name,Song_Price,Song_Code,Song_Period) " \
+          "values ('{0}','{1}','{2}','{3}','{4}');"\
+        .format(RBT_list[item]['Singer_Name'],RBT_list[item]['Song_Name'],RBT_list[item]
+    ['Song_Price'],RBT_list[item]['Song_Code'],RBT_list[item]['Song_Period'])
+    # print(type(RBT_list[item]['Song_Code']))
+
+    mycursor = mydb.cursor()
+    mycursor.execute(sql)
+    mydb.commit()
